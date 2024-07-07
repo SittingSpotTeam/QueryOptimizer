0b5258139438443c1f778e16c62afc5d63f655c4
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:29:59 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/queryoptimizer/QueryoptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.queryoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryoptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryoptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sittingspot/queryoptimizer/QueryoptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sittingspot.queryoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryoptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
